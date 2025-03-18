526070904df2ada6102933d9818124745002ee84
AfonsoBenedito <afonso.benedito23@gmail.com>
2025-03-18 16:41:12 +0000

    347 - Top K Frequent Elements

---
+++ 347 - Top K Frequent Elements/submitted.py
@@ -0,0 +1,29 @@
+def submitted(nums, target):
+    '''
+    Time complexity:
+    Space complexity:
+    '''
+    count = {}
+    buckets = [[] for i in range(len(nums) + 1)]
+
+    for n in nums:
+        count[n] = count.get(n, 0) + 1
+    for key, value in count.items():
+        buckets[value].append(key)
+
+    result = []
+    for i in range(len(buckets) - 1, 0, -1):
+        for num in buckets[i]:
+            result.append(num)
+            if len(result) == target:
+                return result
+
+
+nums1 = [1,1,1,2,2,3]
+k1 = 2
+
+nums2 = [1]
+k2 = 1
+
+print(f"Nums1: {submited(nums1, k1)}")
+print(f"Nums2: {submited(nums2, k2)}")
